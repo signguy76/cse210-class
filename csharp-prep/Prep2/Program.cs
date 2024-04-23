573019e7f01522067bee1fa5ed30b2ecce0e1a36
signguy76 <coates.neal@gmail.com>
2024-04-23 12:11:35 -0700

    Prep 2 initial requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your percentage grade. ");
+            int percentage = int.Parse(Console.ReadLine());
+            {
+                if (percentage >=90)
+                {
+                    Console.WriteLine("Your grade is an A.");
+                }
+                else if (percentage >=80)
+                {
+                Console.WriteLine("Your grade is a B.");
+                }
+                else if (percentage >=70)
+                {
+                Console.WriteLine("Your grade is a C.");
+                }
+                else if (percentage >=60)
+                {
+                Console.WriteLine("Your grade is a D.");
+                }
+                else 
+                {
+                    Console.WriteLine("Sorry, your grade is an F.");
+                }
+            }
     }
 }
