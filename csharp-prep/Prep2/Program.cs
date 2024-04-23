1500c62b4fefbba551f6937204436aa23b9ad284
signguy76 <coates.neal@gmail.com>
2024-04-23 12:34:35 -0700

    Change to letter as variable

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,32 +1,48 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.Write("Please enter your percentage grade. ");
-            int percentage = int.Parse(Console.ReadLine());
-            {
-                if (percentage >=90)
-                {
-                    Console.WriteLine("Your grade is an A.");
-                }
-                else if (percentage >=80)
-                {
-                Console.WriteLine("Your grade is a B.");
-                }
-                else if (percentage >=70)
-                {
-                Console.WriteLine("Your grade is a C.");
-                }
-                else if (percentage >=60)
-                {
-                Console.WriteLine("Your grade is a D.");
-                }
-                else 
-                {
-                    Console.WriteLine("Sorry, your grade is an F.");
-                }
-            }
+        string letter = "";
+        Console.Write("What is your grade percentage? ");
+        string yourGrade = Console.ReadLine();
+        int yourGradeNumber = int.Parse(yourGrade);
+        // Test parse 
+        Console.Write($"At {yourGradeNumber}%, ");
+        if (yourGradeNumber > 90 || yourGradeNumber == 90)
+        {
+            letter = "A";
+            Console.WriteLine($"Congradulations, you passed the course.");
+        }
+        else if (yourGradeNumber < 90 && yourGradeNumber > 80 || yourGradeNumber == 80)
+        {
+            letter = "B";
+
+            Console.WriteLine($"Congradulations, you passed the course.");
+        }
+        else if (yourGradeNumber < 80 && yourGradeNumber > 70 || yourGradeNumber == 70)
+        {
+            letter = "C";
+            Console.WriteLine($"Congradulations, you passed the course.");
+        }
+        else if (yourGradeNumber < 70 && yourGradeNumber > 60 || yourGradeNumber == 60)
+        {
+            letter = "D";
+            Console.WriteLine($"you have not passed. You must have at least 70% to pass the class. Please re-enroll.");
+        }
+        else if (yourGradeNumber < 60)
+        {
+            letter = "F";
+            Console.WriteLine($"you have not passed. You must have at least 70% to pass the class. Please re-enroll.");
+        }
+        {
+
+            Console.WriteLine($"Your final letter grade is {letter}.");
+        }
+
+
     }
 }
