88b2403724dd2e535efb18611ab93a41e4e4e3fc
signguy76 <coates.neal@gmail.com>
2024-04-24 10:05:09 -0700

    Correct if

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,20 +9,26 @@ class Program
         int magicNumber = randomGenerator.Next(1, 100);        
         //Console.Write("What is your magic number? ");
         //    int magicNumber = int.Parse(Console.ReadLine());
-        Console.Write("What is your guess? ");
-            int guessNumber = int.Parse(Console.ReadLine());
-        if (guessNumber == magicNumber)
+        Console.Write("Guess the magic number between 1 and 100? ");
+        int guessNumber = -1;
+        while (guessNumber != magicNumber)
         {
-            Console.WriteLine($"You are right, the number is {guessNumber}");
+            Console.WriteLine("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
 
-        }
-        else if (guessNumber < magicNumber )
-        {
-            Console.WriteLine("Guess higher.");
-        }
-        else
-        {
-            Console.WriteLine("Guess lower.");
+
+            if (guessNumber == magicNumber)
+            {
+                Console.WriteLine($"You are right. The magic number is {magicNumber}.");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Guess higher.");
+            }
+            else
+            {
+                Console.WriteLine("Guess lower.");
+            }
         }
 
     }
