866329319b8163c17f5c231734b8f839256a77db
signguy76 <coates.neal@gmail.com>
2024-04-23 21:27:50 -0700

    Add random number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);        
+        //Console.Write("What is your magic number? ");
+        //    int magicNumber = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+            int guessNumber = int.Parse(Console.ReadLine());
+        if (guessNumber == magicNumber)
+        {
+            Console.WriteLine($"You are right, the number is {guessNumber}");
+
+        }
+        else if (guessNumber < magicNumber )
+        {
+            Console.WriteLine("Guess higher.");
+        }
+        else
+        {
+            Console.WriteLine("Guess lower.");
+        }
+
     }
 }
