1a48aea55163f5910f41e6fb049a95e796dff544
signguy76 <coates.neal@gmail.com>
2024-04-25 11:20:10 -0700

    Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Define Functions
+        // Welcome
+        DisplayWelcomeMessage();
+
+        // ID name
+        string inputName = PromptUserName();
+
+        // ID favorite number
+        int userNumber = PromptUserNumber();
+
+        // Square number
+        int squaredNumber = SquareNumber(userNumber);
+
+
+        // Display results
+        DisplayResult(inputName, squaredNumber);
+
     }
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+
+    }
+    static void DisplayResult(string inputName, int squaredNumber)
+    {
+        Console.WriteLine($"{inputName}, your favorite number squared is {squaredNumber}.");
+        Console.WriteLine("");
+
+    }
+    
 }
