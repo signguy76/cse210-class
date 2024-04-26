3fde4c53f33bc4c6443f34cea657f46832ab975d
signguy76 <coates.neal@gmail.com>
2024-04-25 17:08:50 -0700

    Change >=

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,28 +7,29 @@ class Program
 
     {
         string letter = "";
+        Console.WriteLine("");
         Console.Write("What is your grade percentage? ");
         string yourGrade = Console.ReadLine();
         int yourGradeNumber = int.Parse(yourGrade);
         // Test parse 
         Console.Write($"At {yourGradeNumber}%, ");
-        if (yourGradeNumber > 90 || yourGradeNumber == 90)
+        if (yourGradeNumber >= 90)
         {
             letter = "A";
             Console.WriteLine($"Congradulations, you passed the course.");
         }
-        else if (yourGradeNumber < 90 && yourGradeNumber > 80 || yourGradeNumber == 80)
+        else if (yourGradeNumber >= 80)
         {
             letter = "B";
 
             Console.WriteLine($"Congradulations, you passed the course.");
         }
-        else if (yourGradeNumber < 80 && yourGradeNumber > 70 || yourGradeNumber == 70)
+        else if (yourGradeNumber >= 70)
         {
             letter = "C";
             Console.WriteLine($"Congradulations, you passed the course.");
         }
-        else if (yourGradeNumber < 70 && yourGradeNumber > 60 || yourGradeNumber == 60)
+        else if (yourGradeNumber >= 60)
         {
             letter = "D";
             Console.WriteLine($"you have not passed. You must have at least 70% to pass the class. Please re-enroll.");
@@ -41,6 +42,7 @@ class Program
         {
 
             Console.WriteLine($"Your final letter grade is {letter}.");
+            Console.WriteLine("");
         }
 
 
