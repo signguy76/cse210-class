611dcf579fe30db3636675ce04efbb4ced7afb9e
signguy76 <coates.neal@gmail.com>
2024-05-30 10:02:20 -0700

    Compile Product.cs

---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,34 @@
+public class Product
+{
+    private string _name;
+    private string _productId;
+    private double _price;
+    private int _quantity;
+
+    public Product(string name, string productId, double price, int quantity)
+    {
+        this._name = name;
+        this._productId = productId;
+        this._price = price;
+        this._quantity = quantity;
+    }
+
+    public double GetTotalCost()
+    {
+        return _price * _quantity;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetProductId()
+    {
+        return _productId;
+    }
+    public int GetQuantity()
+    {
+        return _quantity;
+    }
+}
