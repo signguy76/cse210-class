40f76e2923c7048a14bf1c098b881c2b940e834d
signguy76 <coates.neal@gmail.com>
2024-05-30 09:46:36 -0700

    Submittle for FinalProject

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        Console.WriteLine("This assignment was turned in with .docx files.");
     }
 }
