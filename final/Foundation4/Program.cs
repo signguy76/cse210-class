e408a0432c9940077a095f443b0bbdb2bc211188
signguy76 <coates.neal@gmail.com>
2024-06-02 16:18:00 -0700

    Add whitespace

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -12,10 +12,15 @@ public class Program
             new Cycling(new DateTime(2023, 11, 4), 45, 15.0),
             new Swimming(new DateTime(2023, 11, 5), 60, 20)
         };
+        {
+            Console.WriteLine("Activity List:");
+        }
 
         foreach (var activity in activities)
         {
+            
             Console.WriteLine(activity.GetSummary());
+            Console.WriteLine();
         }
     }
 }
